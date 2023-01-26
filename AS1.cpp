6876e836d709a2e261d45426f709d7605145384e
kitorer <vikkhuang1234@gmail.com>
2023-01-26 16:43:44 -0500

    finish

--- AS1.cpp
+++ AS1.cpp
@@ -4,6 +4,7 @@
 #include <bits/stdc++.h>
 #include <thread>
 #include <mutex>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,19 +15,17 @@ int main()
 {
     ofstream myfile;
     myfile.open("primes.txt");
-    auto start = std::chrono::high_resolution_clock::now();
-
+    
     multi_thread_sieve(myfile);
+    single_thread_sieve(myfile);
 
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-    cout << "Time taken by function: "
-         << duration.count() << " milliseconds" << endl;
+    myfile.close();
 }
 
 void multi_thread_sieve(ofstream &myfile)
 {
-    // normal sieve of eratosthenes
+    auto start = std::chrono::high_resolution_clock::now();
+
     int n = 100000000;
     int *prime = new int[n + 1];
     for (int i = 0; i <= n; i++)
@@ -34,30 +33,36 @@ void multi_thread_sieve(ofstream &myfile)
     prime[0] = 0;
     prime[1] = 0;
 
-    // multi-thread sieve of eratosthenes assign each thread a range of numbers to check using 8 threads
     int num_threads = 8;
     int range = n / num_threads;
-    thread threads[num_threads];
+    vector<thread> threads;
     for (int i = 0; i < num_threads; i++)
     {
-        threads[i] = thread([=, &prime] {
-            for (int j = 2; j * j <= range; j++)
+        int start = i * range;
+        int end = (i + 1) * range;
+        threads.push_back(thread([=] {
+            for (int j = 2; j * j <= end; j++)
             {
                 if (prime[j] == 1)
                 {
-                    for (int k = j * j; k <= range; k += j)
+                    for (int k = max(j * j, (start + j - 1) / j * j); k <= end; k += j)
+                    {
                         prime[k] = 0;
+                    }
                 }
             }
-        });
+        }));
     }
-    // join the threads with the main thread
-    for (int i = 0; i < num_threads; i++)
+
+    for (int i = 0; i < threads.size(); i++)
     {
         threads[i].join();
     }
-    
-    //print last 10 primes
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+    // print last 10 primes
     int prime_count = 0;
     long int sum = 0;
     vector<long int> list;
@@ -71,18 +76,26 @@ void multi_thread_sieve(ofstream &myfile)
                 list.insert(list.begin(), i);
         }
     }
+    sort(list.begin(), list.end());
 
     list.insert(list.begin(), sum);
     list.insert(list.begin(), prime_count);
+    list.insert(list.begin(), duration.count());
+    
     for (int i = 0; i < list.size(); i++)
     {
-        myfile << list[i] << " ";
+        myfile << list[i];
+        if (i == 0)
+            myfile << "ms" << " ";
+        else
+            myfile << " ";
     }
+    myfile << endl;
 }
 
 void single_thread_sieve(ofstream &myfile)
 {
-    // normal sieve of eratosthenes
+    auto start = std::chrono::high_resolution_clock::now();
     int n = 100000000;
     int *prime = new int[n + 1];
     for (int i = 0; i <= n; i++)
@@ -97,8 +110,10 @@ void single_thread_sieve(ofstream &myfile)
                 prime[j] = 0;
         }
     }
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 
-    //print last 10 primes
+    // print last 10 primes
     int prime_count = 0;
     long int sum = 0;
     vector<long int> list;
@@ -115,8 +130,13 @@ void single_thread_sieve(ofstream &myfile)
 
     list.insert(list.begin(), sum);
     list.insert(list.begin(), prime_count);
+    list.insert(list.begin(), duration.count());
     for (int i = 0; i < list.size(); i++)
     {
-        myfile << list[i] << " ";
+        myfile << list[i];
+        if (i == 0)
+            myfile << "ms" << " ";
+        else
+            myfile << " ";
     }
 }
